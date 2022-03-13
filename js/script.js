9ce43dcee8902c94a3d51c91efe09231b1d5d908
lmarinovv <la4ezar71@abv.bg>
2022-03-13 18:04:16 +0100

    implemented filterizing image gallery(dropdown nav)

--- js/script.js
+++ js/script.js
@@ -120,3 +120,29 @@ const headerObserver = new IntersectionObserver(stickyNav, {
 headerObserver.observe(sectionHero);
 
 const gallery = document.querySelector(".section-gallery");
+
+$(document).ready(function () {
+  $(".gallery-search-button").click(function () {
+    const value = $(this).attr("data-filter");
+    if (value === "all") {
+      $(".filtr-item").show("12000");
+    } else {
+      $(".filtr-item").not(`.${value}`).hide("500");
+      $(".filtr-item").filter(`.${value}`).show("500");
+    }
+  });
+});
+
+//sidebar
+
+$(".feat-btn").click(function () {
+  $("nav ul .feat-show").toggleClass("show");
+  $("nav ul .first").toggleClass("rotate");
+});
+$(".serv-btn").click(function () {
+  $("nav ul .serv-show").toggleClass("show1");
+  $("nav ul .second").toggleClass("rotate");
+});
+$("nav ul li").click(function () {
+  $(this).addClass("active").siblings().removeClass("active");
+});
